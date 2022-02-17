a7591e3379bbd97bb5446d862a7240178fdeac26
Joalison Matheus <joalison.ferreira@academico.ifpb.edu.br>
2022-02-17 16:06:33 -0300

    added comments

--- CafeAppLis/js/cardapio.js
+++ CafeAppLis/js/cardapio.js
@@ -1,24 +1,25 @@
-import itens from './model/dataset.js';
-import foodsModel from './model/foods.js';
+import itens from './model/dataset.js'; //importanto array de json do arquivo dataset.js, ele é exportado como dataset, mas é declarado aqui como itens 
+import foodsModel from './model/foods.js'; //importando foodsModel do arquivo foods.js
 
-foodsModel.load(itens);
-let foods = foodsModel.readAll();
+foodsModel.load(itens); // está guardando no localStorage o array de json exportado do arquivo dataset.js
+let foods = foodsModel.readAll(); // foods guarda os dados que estão salvos no localStorage, que são chamados da função readAll();
 
-function initFoodsCard () {
-  
-  for (let item of foods) {
+function initFoodsCard() { // criando função initFoodsCard que cria os cards dos jsons que já estão salvos em localStorage
 
-    const view = createFoodCardItem(item);
-  
+  for (let item of foods) { // percorre o array de foods e itera sobre os seus atributos, retornando 
+
+    const view = createFoodCardItem(item); // foodCard guarda os cards criados na função createFoodCardItem
+
     //let itensCardapio = document.querySelector('.itens-cardapio');
-    let itensCardapio = document.getElementById("itens-cardapio");
-    itensCardapio.insertAdjacentHTML('beforeend', view);
-  }
-}
+    let itensCardapio = document.getElementById("itens-cardapio"); // itensCardapio guarda a div que tem como id itens-cardapio
+    itensCardapio.insertAdjacentHTML('beforeend', view); // colocando os cards do cardapio dentro da div que tem como classe "itens-cardapio",
+    // a função insertAdjacentHTML serve para inserir algo em uma posição especificada
+  } // fechamento do for of
+} // fechamento da função initFoodsCard
 
-function createFoodCardItem (item) {
+function createFoodCardItem(item) { // criando função que gera os cards html que vão aparecer em tela
 
-    const view = `<div class="col-3 card my-1 mx-1 py-1">
+  const view = `<div class="col-3 card my-1 mx-1 py-1">
                     <img src="${item.imagem}" class="card-img-top" alt="...">
   
                     <div class="card-body">
@@ -26,26 +27,27 @@ function createFoodCardItem (item) {
                       <p class="card-text">${item.descricao}</p>
                       <a href="#" class="btn btn-primary">Comprar</a>
                     </div>
-                  </div>`;
+                  </div>`; // view guarda os cards que vão aparecer na tela, cada card tem imagem, nome e descrição
 
-    return view;
-}
+  return view; // função retorna a constante view que guarda o html do card que vai ser mostrado em tela
+} // fechamento da função createFoodCardItem
 
 
 // Captar o evento de submissão do formulário e adicionar o item no cartão (card).
 // const foodForm = document.querySelector('#foodForm');
-const foodForm = document.getElementById("foodForm");
+const foodForm = document.getElementById("foodForm"); // foodForm guarda o form que tem como id foodForm
 
-foodForm.onsubmit = function (event) {
+foodForm.onsubmit = function (event) { //criando função para o submit do formulário
   // Previnir que o modal fique abrindo e fechando em loop.
-  event.preventDefault();
+  event.preventDefault(); // prevenindo o evento de submit do formulário, caso ele não seja preenchido 
 
-  let newFood = Object.fromEntries(new FormData(foodForm));
-  foodsModel.create(newFood);
+  let newFood = Object.fromEntries(new FormData(foodForm)); // guarda os dados do formulário como um objeto json
+  foodsModel.create(newFood); // adicionando os dados do formulário no localStorage
 
-  const foodCard = createFoodCardItem(newFood);
-  let itensCardapio = document.getElementById("itens-cardapio");
-  itensCardapio.insertAdjacentHTML('beforeend', foodCard);
-}
+  const foodCard = createFoodCardItem(newFood); // foodCard guarda os cards criados na função createFoodCardItem
+  let itensCardapio = document.getElementById("itens-cardapio"); // itensCardapio guarda a div que tem como id itens-cardapio
+  itensCardapio.insertAdjacentHTML('beforeend', foodCard);  // colocando os cards do cardapio dentro da div que tem como classe "itens-cardapio",
+  // a função insertAdjacentHTML serve para inserir algo em uma posição especificada
+} // fechamento da função foodForm.onsubmit
 
-initFoodsCard();
+initFoodsCard(); // chamada da função initFoodsCard
--- CafeAppLis/js/model/dataset.js
+++ CafeAppLis/js/model/dataset.js
@@ -1,32 +1,32 @@
-const dataset = [
-  {
-    id: 1,
-    name: 'Hambúrguer',
-    image: 'images/hamburguer.jpg',
-    description:
-      'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
-  },
-  {
-    id: 2,
-    name: 'Sanduíche',
-    image: 'images/sanduiche.jpg',
-    description:
-      'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
-  },
-  {
-    id: 3,
-    name: 'Café',
-    image: 'images/cafe.jpg',
-    description:
-      'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
-  },
-  {
-    id: 4,
-    name: 'Suco',
-    image: 'images/suco.jpg',
-    description:
-      'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
-  },
-];
+const dataset = [ //criando uma constante com um array de json que será usado com os dados para preencher os cards
+  { // abertura da chave do json
+    id: 1, // propriedade id com o valor 1, que é um number
+    name: 'Hambúrguer', // propriedade name com valor "Hambúrguer", que é uma string, essa imagem está guardada na pasta imagem
+    image: 'images/hamburguer.jpg', // propriedade image com o endereço relativo da imagem, que é uma string
+    description: // propriedade description com valor da descrição, que é uma string
+      'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado', // string com o texto da descrição
+  }, // fechamento da chave do json
+  { // abertura da chave do json
+    id: 2, // propriedade id com o valor 2, que é um number
+    name: 'Sanduíche', // propriedade name com valor "Sanduíche", que é uma string
+    image: 'images/sanduiche.jpg', // propriedade image com o endereço relativo da imagem, que é uma string, essa imagem está guardada na pasta imagem
+    description: // propriedade description com valor da descrição, que é uma string
+      'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.', // string com o texto da descrição
+  }, // fechamento da chave do json
+  { // abertura da chave do json
+    id: 3, // propriedade id com o valor 3, que é um number
+    name: 'Café', // propriedade name com valor "Café", que é uma string
+    image: 'images/cafe.jpg', // propriedade image com o endereço relativo da imagem, que é uma string, essa imagem está guardada na pasta imagem
+    description: // propriedade description com valor da descrição, que é uma string
+      'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.', // string com o texto da descrição
+  }, // fechamento da chave do json
+  { // abertura da chave do json
+    id: 4, // propriedade id com o valor 4, que é um number
+    name: 'Suco', // propriedade name com valor "Suco", que é uma string
+    image: 'images/suco.jpg', // propriedade image com o endereço relativo da imagem, que é uma string, essa imagem está guardada na pasta imagem
+    description: // propriedade description com valor da descrição, que é uma string
+      'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.', // string com o texto da descrição
+  }, // fechamento da chave do json
+]; //fechamento do array de json
 
-export default dataset;
+export default dataset; // exportando dataset como default, caso já tenha alguma variável com o nome dataset aonde ele for importado, vai poder ser mudado o nome da variável para o que a pessoa quiser
